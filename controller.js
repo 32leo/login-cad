5a25b9f15846117777e3684dcfb0da6ace48d3af
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-07-04 11:23:21 -0300

    realiza as alterações nos arquivos

--- controller.js
+++ controller.js
@@ -1,15 +1,20 @@
 // ----------------------------------------------------------------------------------------------------------------------------
 function acessar(){
-   let loginEmailm = document.getElementById('loginEmail').value;
+   let loginEmail = document.getElementById('loginEmail').value;
    let loginSenha = document.getElementById('loginSenha').value;
 
-      if(!loginEmailm || !loginSenha){
+      if(!loginEmail || !loginSenha){
          alert('favor preencher todos os campos');
 
-      }
+      }else{
        // else{ alert('campos preenchisdos com sucesso'); }
-       window.location.hrefc = 'cadrastro.html'
+       window.location.hrefc = 'cadastro.html'
+      }
 }
 
 // FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
 //------------------------------------------------------------------------------------------------------------------------
+var dadosLista = [];
+function salarUser(){
+   
+}
