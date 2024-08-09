e86177b410788b9f83bf359b4ac93dd2196e2c11
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-08-09 07:20:15 -0300

    realiza as mudanças no arquivo

--- controller.js
+++ controller.js
@@ -20,17 +20,35 @@ function salvarUser(){
 
    if(nomeUser){
       dadosLista.push(nomeUser);
-      console.log(dadosLista);
-
+      // console.log(dadosLista);
+      criarLista()
+      document.getElementById('nomeUser').value = "";
    }else{
       alert('porfavor iforme um nome para cadastro');
    }
 
 
 }
-
+// funçao para criar listas de usuario
+// ----------------------------------------------------------------------------------------------------------------------
 function criarLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr> <th>Nome Usuario</th><th>Acões</th></tr>";
-    
+    for(let i=0;i <=(dadosLista.length - 1);i++){
+         tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+      document.getElementById('tabela').innerHTML = tabela;
+    }
+
+}
+// função para editar nomes de listas 
+// ----------------------------------------------------------------------------------------------------------------------
+function editar(i){
+   document.getElementById('nomeUser').value = dadosLista[(i-1)]
+   dadosLista.splice(dadosLista[(i - 1)],1);
+}  
+// função para excluir nome de lista 
+// ----------------------------------------------------------------------------------------------------------------------
+function excluir(i){
+   dadosLista.splice((i - 1), 1);
+   document.getElementById('tabela').deleteRow(i);
 
 }
